6f332a8569791ff115eb23b2845d2820d07e0f43
jai570 <sharmajaikishan60@gmail.com>
2024-10-26 08:45:56 +0530

    first commit

---
+++ Readme.md
@@ -0,0 +1,12 @@
+# Node version used:
+18.15.0
+
+# To install the dependencies use the following command:
+npm install
+
+# To run the project:
+npm start
+
+# Technologies used:
+React.js, TailwindCSS, axios, react-icons and react toastify
+
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+export default function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <h1> simple react project</h1>
+      </BrowserRouter>
+    </div>
+  )
+}
